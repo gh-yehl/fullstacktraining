0ad0aa50eb6e763e4415694d01565a4e30786f92
HONGLIYE <yehl@cn.ibm.com>
2019-10-30 22:25:44 +0800

    good to go

--- src/main/java/com/fullstack/ApplicationBoot.java
+++ src/main/java/com/fullstack/ApplicationBoot.java
@@ -2,10 +2,12 @@ package com.fullstack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ApplicationBoot {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationBoot.class, args);
